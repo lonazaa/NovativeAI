0e9dc34fdc6ed98f6eb13cf1863f6d70ef7f9ee2
Lonazetti <rokas.sumas@gmail.com>
2025-07-12 23:04:53 +0300

    Update site URL to HTTPS for secure connections

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://novativeai.com',
+  site: 'https://novativeai.com',
   build: {
     assets: 'assets',
   },
